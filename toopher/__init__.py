29acbcba7159d32e017980773450e5a8eb367ca4
Seth Holloway <smholloway@gmail.com>
2013-09-13 11:23:18 -0500

    Create a Toopher User-Agent string

--- toopher/__init__.py
+++ toopher/__init__.py
@@ -19,7 +19,7 @@ class ToopherApi(object):
                   'user_name': user_name}
 
         params.update(kwargs)
-        
+
         result = self._request(uri, "POST", params)
         return PairingStatus(result)
 
@@ -27,16 +27,16 @@ class ToopherApi(object):
         uri = BASE_URL + "/pairings/create/sms"
         params = {'phone_number': phone_number,
                   'user_name': user_name}
-       
+
         if phone_country:
             params['phone_country'] = phone_country
 
         result = self._request(uri, "POST", params)
         return PairingStatus(result)
-        
+
     def get_pairing_status(self, pairing_id):
         uri = self.base_url + "/pairings/" + pairing_id
-        
+
         result = self._request(uri, "GET")
         return PairingStatus(result)
 
@@ -48,13 +48,13 @@ class ToopherApi(object):
             params['action_name'] = action_name
 
         params.update(kwargs)
-            
+
         result = self._request(uri, "POST", params)
         return AuthenticationStatus(result)
 
     def get_authentication_status(self, authentication_request_id):
         uri = self.base_url + "/authentication_requests/" + authentication_request_id
-        
+
         result = self._request(uri, "GET")
         return AuthenticationStatus(result)
 
@@ -63,23 +63,24 @@ class ToopherApi(object):
         params = {'otp' : otp}
         result = self._request(uri, "POST", params)
         return AuthenticationStatus(result)
-    
+
     def _request(self, uri, method, params=None):
         data = urllib.urlencode(params or {})
-        
-        resp, content = self.client.request(uri, method, data)
+        header_data = {'User-Agent':'Toopher-Python/{}'.format(VERSION)}
+
+        resp, content = self.client.request(uri, method, data, headers=header_data)
         if resp['status'] != '200':
             try:
                 error_message = json.loads(content)['error_message']
             except Exception:
                 error_message = content
             raise ToopherApiError(error_message)
-        
+
         try:
             result = json.loads(content)
         except Exception, e:
             raise ToopherApiError("Response from server could not be decoded as JSON: %s" % e)
-        
+
         return result
 
 
@@ -88,7 +89,7 @@ class PairingStatus(object):
         try:
             self.id = json_response['id']
             self.enabled = json_response['enabled']
-            
+
             user = json_response['user']
             self.user_id = user['id']
             self.user_name = user['name']
@@ -96,7 +97,7 @@ class PairingStatus(object):
             raise ToopherApiError("Could not parse pairing status from response" + e.message)
 
         self._raw_data = json_response
-        
+
     def __nonzero__(self):
         return self.enabled
 
@@ -112,7 +113,7 @@ class AuthenticationStatus(object):
             self.granted = json_response['granted']
             self.automated = json_response['automated']
             self.reason = json_response['reason']
-            
+
             terminal = json_response['terminal']
             self.terminal_id = terminal['id']
             self.terminal_name = terminal['name']
@@ -129,3 +130,4 @@ class AuthenticationStatus(object):
 
 
 class ToopherApiError(Exception): pass
+
