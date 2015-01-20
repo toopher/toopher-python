9b1e7e829ea05cac7c61fb5505e28e2cf8b9f801
Amanda Steinwedel <amanda@toopher.com>
2015-01-20 09:51:19 -0600

    Make sure we save self._raw_data appropriately with instance methods

--- toopher/__init__.py
+++ toopher/__init__.py
@@ -332,6 +332,7 @@ class Pairing(object):
         self.pending = result['pending']
         user = result['user']
         self.user_name = user['name']
+        self._raw_data = result
 
     def get_qr_code_image(self, api):
         url = api.base_url + '/qr/pairings/' + self.id
@@ -380,6 +381,7 @@ class AuthenticationRequest(object):
         self.reason = result['reason']
         terminal = result['terminal']
         self.terminal_name = terminal['name']
+        self._raw_data = result
 
 
 class UserTerminal(object):
@@ -409,6 +411,7 @@ class UserTerminal(object):
         self.name_extra = result["name_extra"]
         user = result["user"]
         self.user_name = user["name"]
+        self._raw_data = result
 
 
 class User(object):
@@ -433,14 +436,17 @@ class User(object):
         result = api._request(url, 'GET')
         self.name = result['name']
         self.disable_toopher_auth = result['disable_toopher_auth']
+        self._raw_data = result
 
     def enable(self, api):
         api._set_toopher_disabled_for_user(self.name, False)
         self.disable_toopher_auth = False
+        self._raw_data['disable_toopher_auth'] = False
 
     def disable(self, api):
         api._set_toopher_disabled_for_user(self.name, True)
         self.disable_toopher_auth = True
+        self._raw_data['disable_toopher_auth'] = True
 
     def reset(self, api):
         return api.reset_user(self.name)
